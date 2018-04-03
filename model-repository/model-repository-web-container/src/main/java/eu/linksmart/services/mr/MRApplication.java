c29eb63b771dfbf5be1e7026e01fb294b50a8936
Rossen Rashev <rossen.rachev@fit.fraunhofer.de>
2018-04-03 14:53:44 +0200

    sc registration implemented

--- model-repository/model-repository-web-container/src/main/java/eu/linksmart/services/mr/MRApplication.java
+++ model-repository/model-repository-web-container/src/main/java/eu/linksmart/services/mr/MRApplication.java
@@ -1,84 +1,126 @@
 package eu.linksmart.services.mr;
 
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+//import eu.linksmart.lc;
+//import eu.linksmart.lc.sc.client;
+
 public class MRApplication {
-	
+
 	private final Logger LOG = LoggerFactory.getLogger(MRApplication.class);
-	
+
 	private String host;
-	
+
 	private int port;
-	
+
 	private String pathContext = "model-repository";
-	
+
 	private final String PROPERTY_FILE = "/model-repository.properties";
-	
+
 	private RepositoryWebContainer container = null;
+
+	private String SERVICE_FILE = "/service.json";
+
+	private String SC_BASE_URL = "http://localhost:8082/sc";
+
+	private String serviceID = "example-MR";
+
+	private boolean scRegistered = false;
 
 	public static void main(String[] args) {
 		new MRApplication();
 	}
-	
+
 	public MRApplication() {
-		
+
 		LOG.info("------------------------------------------------------------------------------------");
 		LOG.info("staring the model repository");
 		LOG.info("------------------------------------------------------------------------------------");
-		
+
 		try {
 			readDefaults();
 		} catch (Exception e) {
 			LOG.error("unable to read configuration file [" + PROPERTY_FILE + "] " + e.getMessage());
 			System.exit(1);
 		}
-		
+
 		container = new RepositoryWebContainer(this);
 		container.start();
-		
+
+
+		try {
+			String jsonService = new String(Files.readAllBytes(Paths.get(getClass().getResource(SERVICE_FILE).toURI())), Charset.defaultCharset());
+
+			if (SC_BASE_URL != null && !SC_BASE_URL.isEmpty()) {
+				Thread.sleep(1000);
+				scRegistered = createRegistrationInSC(jsonService);
+			}
+		} catch (Exception e) {
+			LOG.error("unable to read service defenition file [" + SERVICE_FILE + "] " + e.getMessage());
+			System.exit(1);
+		}
+
 		Runtime.getRuntime().addShutdownHook(new Thread() {
-		    public void run() {
-		    	shutdown();
-		    }
+			public void run() {
+				shutdown();
+			}
 		});
 	}
-	
+
 	protected void shutdown() {
 		LOG.info("------------------------------------------------------------------------------------");
 		LOG.info("shutting down the model repository");
 		LOG.info("------------------------------------------------------------------------------------");
+
+		if (scRegistered) deleteRegistrationInSC(serviceID);
 		//ModelRepository.getInstance().closePersistence();
 		container.stopContainer();
 		container.interrupt();
-		try { Thread.sleep(1000); } catch (InterruptedException e) { }
+//		try { Thread.sleep(1000); } catch (InterruptedException e) { }
 		LOG.info("model repository server terminated");
 		Runtime.getRuntime().runFinalization();
 		System.exit(0);
 		//Runtime.getRuntime().halt(0); // this method should be used very carefully since it doesn't gracefully shutdown the virtual machine
 	}
-	
+
 	private void readDefaults() throws Exception {
-		
+
 		Properties prop = new Properties();
-		
+
 		InputStream inputStream = getClass().getResourceAsStream(PROPERTY_FILE);
 		prop.load(inputStream);
-		
+
 		this.host = prop.getProperty("model.repository.host");
 		this.port = Integer.parseInt(prop.getProperty("model.repository.port"));
 		this.pathContext = prop.getProperty("model.repository.path");
-		
+		this.serviceID = prop.getProperty("model.repository.serviceID");
+		this.SERVICE_FILE = prop.getProperty("model.repository.scFile");
+		this.SC_BASE_URL = prop.getProperty("model.repository.scURL");
+
 		LOG.info("using host: " + this.host);
 		LOG.info("using port: " + this.port);
 		LOG.info("using path: " + this.pathContext);
-		
-        inputStream.close();
+		LOG.info("using serviceID: " + this.serviceID);
+		LOG.info("using scFile: " + this.SERVICE_FILE);
+		LOG.info("using scURL: " + this.SC_BASE_URL);
+
+		inputStream.close();
 	}
-	
+
 	protected final String getHost() {
 		return host;
 	}
@@ -91,4 +133,62 @@ public class MRApplication {
 		return pathContext;
 	}
 
+	public boolean createRegistrationInSC(String serviceJson) {
+		LOG.info("------------------------------------------------------------------------------------");
+		LOG.info("registering in service catalog");
+		LOG.info("------------------------------------------------------------------------------------");
+		Client client = Client.create();
+		try {
+
+			String interface_option = "/" + serviceID;
+			String scAddress = SC_BASE_URL + interface_option;
+
+			WebResource webResourceClient = client.resource(scAddress);
+
+			ClientResponse response = webResourceClient.type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).put(ClientResponse.class, serviceJson);
+
+			boolean success = (response.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL);
+
+			if (success) {
+				return success;
+			} else {
+				String error = response.getStatusInfo().getReasonPhrase();
+				throw new Exception("failed to add a service : status code: " + response.getStatusInfo().getStatusCode() + " - reason: " + error);
+			}
+
+		} catch (Exception e) {
+			System.err.println("service catalog error: reason: " + e.getMessage());
+			return false;
+		}
+	}
+
+	public boolean deleteRegistrationInSC(String serviceID) {
+		LOG.info("------------------------------------------------------------------------------------");
+		LOG.info("deleting from service catalog");
+		LOG.info("------------------------------------------------------------------------------------");
+		Client client = Client.create();
+		try {
+
+			String interface_option = "/" + serviceID;
+			String scAddress = SC_BASE_URL + interface_option;
+			LOG.info("dele address: " + scAddress);
+
+			WebResource webResourceClient = client.resource(scAddress);
+
+			ClientResponse response = webResourceClient.accept(MediaType.TEXT_PLAIN).delete(ClientResponse.class);
+
+			boolean success = (response.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL);
+
+			if (success) {
+				return success;
+			} else {
+				String error = response.getStatusInfo().getReasonPhrase();
+				throw new Exception("failed to remove a service : status code: " + response.getStatusInfo().getStatusCode() + " - reason: " + error);
+			}
+
+		} catch (Exception e) {
+			System.err.println("service catalog error: reason: " + e.getMessage());
+			return false;
+		}
+	}
 }
